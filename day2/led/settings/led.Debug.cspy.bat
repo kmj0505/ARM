aab930e6e89d465d591cdd2375c87f669b13a5af
kmj0505 <menge0505@naver.com>
2023-02-13 18:47:35 +0900

    led 1~8번-> 8~1번까지 이동하며 켜기

--- day2/led/settings/led.Debug.cspy.bat
+++ day2/led/settings/led.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files\IAR Systems\Embedded Workbench 9.1\common\bin\cspybat" -f "C:\Users\IoT13\Documents\iar\led\settings\led.Debug.general.xcl" --backend -f "C:\Users\IoT13\Documents\iar\led\settings\led.Debug.driver.xcl" 
+"C:\Program Files\IAR Systems\Embedded Workbench 9.1\common\bin\cspybat" -f "C:\project\ARM\day2\led\settings\led.Debug.general.xcl" --backend -f "C:\project\ARM\day2\led\settings\led.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files\IAR Systems\Embedded Workbench 9.1\common\bin\cspybat" -f "C:\Users\IoT13\Documents\iar\led\settings\led.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\IoT13\Documents\iar\led\settings\led.Debug.driver.xcl" 
+"C:\Program Files\IAR Systems\Embedded Workbench 9.1\common\bin\cspybat" -f "C:\project\ARM\day2\led\settings\led.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\project\ARM\day2\led\settings\led.Debug.driver.xcl" 
 
 @echo off 
 :end
--- day2/led/settings/led.Debug.cspy.ps1
+++ day2/led/settings/led.Debug.cspy.ps1
@@ -23,9 +23,9 @@
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files\IAR Systems\Embedded Workbench 9.1\common\bin\cspybat" -f "C:\Users\IoT13\Documents\iar\led\settings\led.Debug.general.xcl" --backend -f "C:\Users\IoT13\Documents\iar\led\settings\led.Debug.driver.xcl" 
+& "C:\Program Files\IAR Systems\Embedded Workbench 9.1\common\bin\cspybat" -f "C:\project\ARM\day2\led\settings\led.Debug.general.xcl" --backend -f "C:\project\ARM\day2\led\settings\led.Debug.driver.xcl" 
 }
 else
 {
-& "C:\Program Files\IAR Systems\Embedded Workbench 9.1\common\bin\cspybat" -f "C:\Users\IoT13\Documents\iar\led\settings\led.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\IoT13\Documents\iar\led\settings\led.Debug.driver.xcl" 
+& "C:\Program Files\IAR Systems\Embedded Workbench 9.1\common\bin\cspybat" -f "C:\project\ARM\day2\led\settings\led.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\project\ARM\day2\led\settings\led.Debug.driver.xcl" 
 }
