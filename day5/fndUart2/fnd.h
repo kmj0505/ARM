3d84f5a92e77520c76b8a073885a249fa8397fb2
kmj0505 <menge0505@naver.com>
2023-03-07 18:27:05 +0900

    재업로드

--- day5/fndUart2/fnd.h
+++
@@ -1,8 +0,0 @@
-#ifndef __FND_H__
-#define __FND_H__
-
-void FND_init();
-void display_fnd ( int N );
-void display_digit(int position , int number);
-static void Delay(const uint32_t Count);
-#endif
