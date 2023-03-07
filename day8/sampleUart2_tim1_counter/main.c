d9fefc8242e916f6381f285d4e568930e834e5fb
kmj0505 <menge0505@naver.com>
2023-03-07 19:04:21 +0900

    버튼 눌러서 숫자 1씩 증가하기

---
+++ day8/sampleUart2_tim1_counter/main.c
@@ -0,0 +1,96 @@
+/******************************************************************************
+* Advanced-control timers TIM1
+
+  포트 연결:
+    1)  ARM Cortex-M4 모듈의 포트C (PC0 ~ PC7)를 8핀 케이블를 이용해서
+        Array FND모듈의 SA_A ~ SA_H에 연결한다. (SA_A가 PC0로 연결되야 한다.)
+    2)  ARM Cortex-M4 모듈의 포트C (PC8 ~ PC11)를 4핀 케이블를 이용해서
+        Array FND모듈의 C0 ~ C3에 연결한다. (C0가 PC8로 연결되야 한다.)
+    3)  ARM Cortex-M4 모듈의 포트A의 PA8을 2핀 케이블를 이용해서 
+        PushButton모듈의 BT0에 연결한다.
+******************************************************************************/
+// stm32f4xx의 각 레지스터들을 정의한 헤더파일
+#include "stm32f4xx.h"
+
+
+// delay 함수
+static void Delay(const uint32_t Count)
+{
+  __IO uint32_t index = 0; 
+  for(index = (16800 * Count); index != 0; index--);
+}
+
+// 7-세그먼트 폰트를 배열로 지정한다.
+unsigned char Font[18] = {0x3F, 0X06, 0X5B, 0X4F, 
+                                         0X66, 0X6D, 0X7C, 0X07,
+                                         0X7F, 0X67, 0X77, 0X7C, 
+                                         0X39, 0X5E, 0X79, 0X71, 
+                                         0X08, 0X80};
+
+unsigned int m_cnt;
+
+
+void Segment ( int N )  // Segment 함수 선언
+{
+  int  i ;
+  unsigned char N1000, N100, N10, N1 ;
+  int Buff ;   
+
+  N1000 = N /1000;  // 세그먼트에서 사용하는 천의 자리를 추출
+  Buff = N % 1000 ;
+  N100 = Buff / 100 ; // 세그먼트에서 사용하는 백의자리 추출
+  Buff = Buff % 100;
+  N10 = Buff /10 ;     // 세그먼트에서 사용하는 십의 자리 추출
+  N1 =  Buff % 10 ;    // 세그먼트에서 사용하는 일의 자리 추출      
+
+  for( i = 0 ; i < 30; i++ )
+  {
+    GPIO_Write(GPIOC, Font[N1000]|0x0e00); // 왼쪽 첫 번째 세그먼트를 ON하고, 천의 자리  숫자를 출력해 준다 
+    Delay(1);
+
+    GPIO_Write(GPIOC, Font[N100]|0x0d00); // 왼쪽 두 번째 세그먼트를 ON하고, 백의 자리  숫자를 출력해 준다 
+    Delay(1); 
+
+    GPIO_Write(GPIOC, Font[N10]|0x0b00);  // 왼쪽 세 번째 세그먼트를 ON하고, 십의 자리  숫자를 출력해 준다 
+    Delay(1);
+
+    GPIO_Write(GPIOC, Font[N1]|0x0700);   // 왼쪽 네 번째 세그먼트를 ON하고, 일의 자리  숫자를 출력해 준다 
+    Delay(1); 
+  }
+}
+
+
+int main()
+{
+  GPIO_InitTypeDef   GPIO_InitStructure;
+
+
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC|RCC_AHB1Periph_GPIOA, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
+
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0|GPIO_Pin_1|GPIO_Pin_2|GPIO_Pin_3|
+                                GPIO_Pin_4|GPIO_Pin_5|GPIO_Pin_6|GPIO_Pin_7|
+                                GPIO_Pin_8|GPIO_Pin_9|GPIO_Pin_10|GPIO_Pin_11;
+  GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;             //TIM1_CH1
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource8, GPIO_AF_TIM1);
+
+  TIM_TIxExternalClockConfig(TIM1,TIM_TIxExternalCLK1Source_TI1,TIM_ICPolarity_Rising,0x08);
+
+  TIM_Cmd(TIM1,ENABLE);
+
+  while(1)
+  {
+    m_cnt = TIM_GetCounter(TIM1);
+    Segment(m_cnt);
+  }
+}
